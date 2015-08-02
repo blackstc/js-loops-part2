df90ee321f53cd1673a4bfedd7fb02932b1372d5
Blackstock <charlieblackstock@gmail.com>
2015-08-02 13:51:02 -0600

    add solutions for problems 1-4

--- js/main.js
+++ js/main.js
@@ -1,3 +1,33 @@
 // add scripts
 
 console.log("sanity check!");
+
+var students = ['joe', 'carl', 'rachel', 'derek', 'lily'];
+
+//problem 1
+// for (var i = 0; i < students.length; i++) {
+//   console.log(students[i]);
+// }
+
+//problem 2
+// for (var i = 0; i < students.length; i++) {
+//   if (i % 2 === 0) {
+//     console.log(students[i]);
+//   }
+// }
+
+//problem 3
+// var reverseStudents = students.reverse();
+// for (var i = 0; i < students.length; i++) {
+//   console.log(reverseStudents[i]);
+// }
+
+//problem 4
+for (var i = 0; i < students.length; i++) {
+  if (students[i] === students[0] || students[i] === students[students.length - 1]) {
+    console.log(students[i]);
+  } else {
+    console.log(students[i]);
+    console.log(students[i]);
+  }
+}
